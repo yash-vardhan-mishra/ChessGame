c1e390ae5792e1a205ba4b245b31a397a80ea623
Yash Vardhan <yash-vardhan@hotmail.com>
2021-02-01 04:22:54 +0530

    added the gesture events and functionality

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,19 +1,13 @@
 import React, {useCallback, useState} from 'react';
 import {View, StyleSheet, Dimensions} from 'react-native';
 import {Chess} from 'chess.js';
+
+import {useConst} from './helperFunctions';
+
 import Background from './Background';
+import Piece from './Piece';
 
 const {width} = Dimensions.get('window');
-
-const Board = () => {
-  return (
-    <View style={styles.container}>
-      <Background />
-    </View>
-  );
-};
-
-export default Board;
 
 const styles = StyleSheet.create({
   container: {
@@ -21,3 +15,41 @@ const styles = StyleSheet.create({
     height: width,
   },
 });
+
+const Board = () => {
+  const chess = useConst(() => new Chess());
+  const [state, setState] = useState({
+    player: 'w',
+    board: chess.board(),
+  });
+  const onTurn = useCallback(() => {
+    setState({
+      player: state.player === 'w' ? 'b' : 'w',
+      board: chess.board(),
+    });
+  }, [chess, state.player]);
+  return (
+    <View style={styles.container}>
+      <Background />
+      {state.board.map((row, y) =>
+        row.map((piece, x) => {
+          if (piece !== null) {
+            return (
+              <Piece
+                key={`${x}-${y}`}
+                id={`${piece.color}${piece.type}` as const}
+                startPosition={{x, y}}
+                chess={chess}
+                onTurn={onTurn}
+                enabled={state.player === piece.color}
+              />
+            );
+          }
+          return null;
+        }),
+      )}
+    </View>
+  );
+};
+
+export default Board;
